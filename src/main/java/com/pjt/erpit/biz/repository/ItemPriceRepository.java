d0b8aed02975d716a6899f6e9e5433d1c06c49ff
SowonBae <engel5526@naver.com>
2024-11-07 09:06:27 +0900

    fix itemprice add useyn

--- src/main/java/com/pjt/erpit/biz/repository/ItemPriceRepository.java
+++ src/main/java/com/pjt/erpit/biz/repository/ItemPriceRepository.java
@@ -1,19 +1,21 @@
 package com.pjt.erpit.biz.repository;
 
 import com.pjt.erpit.biz.entity.ItemPrice;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Item Price Repository
  */
 @Repository
 public interface ItemPriceRepository extends JpaRepository<ItemPrice, Long> {
-    ItemPrice findByBuyercdAndItemcd(String buyercd, String itemcd);
+    Optional<ItemPrice> findByBuyercdAndItemcdAndUseyn(@NotNull String buyercd, @NotNull String itemcd, String useyn);
 
     @Query("SELECT ip FROM M_ITEM_PRICE ip " +
             "INNER JOIN M_ITEM i ON ip.itemcd = i.itemcd " +
@@ -22,4 +24,5 @@ public interface ItemPriceRepository extends JpaRepository<ItemPrice, Long> {
             "AND (:buyer IS NULL OR :buyer = '' OR LOWER(ip.buyercd) LIKE LOWER(CONCAT('%', :buyer, '%')) OR LOWER(b.buyernm) LIKE LOWER(CONCAT('%', :buyer, '%'))) " +
             "AND ip.useyn = 'Y'")
             List<ItemPrice> searchItemPrice(@Param("item") String item, @Param("buyer") String buyer);
+
 }
